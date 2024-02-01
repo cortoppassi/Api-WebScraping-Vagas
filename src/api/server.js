072e95a15d209d65cf6bab1f0af609f2c3e3d006
Jonathan <jonathan-516@hotmail.com>
2024-02-01 18:22:06 -0300

    update

--- src/api/server.js
+++ src/api/server.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const app = express();
 const port = 3001;
 app.use(cors());
-app.get('/api/vagas', async (req, res) => {
+app.get('/', async (req, res) => {
   // Launch the browser and open a new blank page
   const browser = await puppeteer.launch({headless: false});
   const page = await browser.newPage();
